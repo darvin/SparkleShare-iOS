819ad075f11ebbfd33de77ef28abe10bfbb93524
Sergey Klimov <dcdarv@gmail.com>
2011-11-15 04:23:35 +0200

    big structure refactoring

---
+++ SparkleShare/Supporting/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SparkleShare
+//
+//  Created by Sergey Klimov on 07.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SparkleShareAppDelegate.h"
+
+int main(int argc, char *argv[]) {
+	@autoreleasepool {
+		return UIApplicationMain( argc, argv, nil, NSStringFromClass([SparkleShareAppDelegate class]) );
+	}
+}
